8e7f898a670b34545605dd2d07ebc638750d34bd
MuindiStephen <stephenmuindi241@gmail.com>
2024-03-12 12:30:39 +0300

    module: dagger-hilt

--- di/src/main/java/com/stevemd/di/module/NetworkModule.kt
+++ di/src/main/java/com/stevemd/di/module/NetworkModule.kt
@@ -2,15 +2,17 @@ package com.stevemd.di.module
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.internal.GsonBuildConfig
+import com.stevemd.di.qualifier.AppBaseUrl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Converter
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -37,8 +39,26 @@ object NetworkModule {
     fun providesConverterFactory(gson: Gson): GsonConverterFactory {
         return GsonConverterFactory.create(gson)
     }
+
+    @Singleton
     @Provides
     fun provideGson(): Gson {
        return GsonBuilder().create()
+    }
+
+    @Singleton
+    @Provides
+    @AppBaseUrl
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        factory: GsonConverterFactory,
+        callAdapter: CallAdapter.Factory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .client(okHttpClient)
+            .addConverterFactory(factory)
+            .addCallAdapterFactory(callAdapter)
+            .build()
     }
 }
